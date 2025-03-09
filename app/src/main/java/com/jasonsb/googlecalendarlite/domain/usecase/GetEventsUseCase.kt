a6b3343533b5265a86de0f18a89a769c0a361a0d
Jason Sebastian <jasonnsebastian@gmail.com>
2025-03-09 10:59:55 +0800

    Create GetEventsUseCase

---
+++ app/src/main/java/com/jasonsb/googlecalendarlite/domain/usecase/GetEventsUseCase.kt
@@ -0,0 +1,9 @@
+package com.jasonsb.googlecalendarlite.domain.usecase
+
+import javax.inject.Inject
+
+/**
+ * Use case to get calendar events.
+ */
+class GetEventsUseCase @Inject constructor() {
+}
